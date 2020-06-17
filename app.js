326b2892603345f26396030659cd8e2cf7cf666a
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-18 00:32:13 +0500

    Javascript chapter 21-25 complete

--- app.js
+++ app.js
@@ -89,11 +89,14 @@ replaceAMessage = replaceAMessage.replace("Ali and Sami are best friends.They pl
 
 // chapter 21-25 Task #11
 
-//  var language = prompt("Please enter your favourite language");
-//  var networkZero = language.toLowerCase();
+ var language = prompt('What is your favourite programming language')
+ var select = language.charAt(0).toUpperCase()
+ var slices = language.slice(1, 10) 
  
-//  document.write("<br/>" + "<br/>" + "User input: " + networK);
-//  document.write("<br/>" + "Lower Case: " + networkZero);
+//  console.log(select + slices);
+ 
+ document.write("<br/>" + "<br/>" + "<b> User input: </b>" + language);
+ document.write("<br/>" + "<b> Title Case: </b>" + select + slices);
 
 // chapter 21-25 Task #12
 
@@ -108,7 +111,7 @@ replaceAMessage = replaceAMessage.replace("Ali and Sami are best friends.They pl
  var num3 = num1 + num2;
  
  document.write("<br/>" + "<br/>" + "<b> Number: </b>" + num)  
- document.write("<br/>" + "<b> Result: </b>" + num3);
+ document.write("<br/>" + "<b> Result: </b>" + num3 + "<br />");
 
 // chapter 21-25 Task #13
 
@@ -124,3 +127,100 @@ replaceAMessage = replaceAMessage.replace("Ali and Sami are best friends.They pl
             alert("Enter a valid  username.");
       }
  }
+
+ // chapter 21-25 Task #14
+
+ var userPrompt = prompt("Welcome to ABC Bakery.What do you want to order sir/ma'ma?");
+ var bakerArr = ["cake", "apple pie","cookie", "chips" , "patties"];
+
+  var myFlag, indeX;
+
+  var changeCase = userPrompt.toUpperCase();
+    // console.log(changeCase);
+  
+  for (var i = 0; i < bakerArr.length; i++) {   
+     
+   var changeCases = bakerArr[i].toUpperCase()
+     
+ 
+    if (changeCase === changeCases) {
+
+      myFlag = true;
+      indeX = i;     
+      
+      break;
+  }
+  
+   else {
+         myFlag = false;
+      }
+     
+ } 
+  
+  if (myFlag === true) {      
+       document.write(" <br /> " + userPrompt + " is available at index " + indeX + " in our bakery <br/>");     
+  }   
+   
+  else {   
+   alert("We are sorry " + userPrompt + " is  not available in our bakery"); 
+  }
+
+
+ // chapter 21-25 Task #15
+
+ var input = prompt("Enter password"); 
+
+      console.log(input.length);      
+
+for (var i = 0; i < 1; i++) {
+
+  if (input !== i && input.length > 6) {
+    alert("password is valid");
+    break;
+  }
+  
+  else if(input.charCodeAt(i) === 38 || input.charCodeAt(i) === 35 ||
+          input.charCodeAt(i) === 64 || input.charCodeAt(i) === 36) {
+            alert('password is incorrect')
+
+           //  ASCII CODE 35(#) 38(%) 64(@) 36($) ASCII CODE
+      }
+  
+   else
+    alert("Password should not start with number and password length greater than 6 ");
+}
+
+ 
+
+ //chapter 21-25 Task #16
+
+ var str = "University of Karachi";    
+ 
+           document.write("<br/>")
+     
+      for(var a = 0; a < 1; a++) {                 
+        document.write(str.toString().split(''));                 
+      }
+
+  //chapter 21-25 Task #17
+
+  var userInput = "Pakistan";
+
+  var lastChar = userInput.charAt(7)
+
+    //  console.log(lastChar);
+
+  document.write("<br /> <br />" + "<b>User input: </b>" + userInput + "<br />")  
+  document.write("<b>Last character of input: </b>" + lastChar + "<br />")  
+
+ // chapter 21-25 Task #18
+
+ var sentence = "The quick brown fox jumps over the lazy dog.";
+ var lowerCase =  sentence.toLowerCase()
+  var searchGlobal = lowerCase.match(/the/g).length  
+   
+   console.log(searchGlobal);   
+  
+  document.write("<br /> " + "<b> Text: </b>" + sentence)
+  document.write("<br />" + "<b> There are </b> " + searchGlobal + ' occurrence(s) of word the')
+                     
